faeb02c4411082e5123cbc5dcd6207d8310f29f0
Novitz Jean-Philippe <novitz@gmail.com>
2021-05-15 01:57:42 +0200

    new (product) crud action completed controller + template

--- src/Controller/Admin/Product/ProductController.php
+++ src/Controller/Admin/Product/ProductController.php
@@ -7,6 +7,7 @@ use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,18 +16,21 @@ class ProductController extends AbstractController
 {
     protected $em;
 
-    public function __construct(EntityManagerInterface $manager){
-        $this->em = $manager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
     }
 
-
     /**
-     * @Route("/admin/productd", name="admin_products_list")
+     * @Route("/admin/product", name="admin_product_index")
      */
     public function index(): Response
     {
+        $list = $this->em->getRepository(Product::class)
+            ->getAll();
+
         return $this->render('admin/product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'list' => $list,
         ]);
     }
 
@@ -56,6 +60,95 @@ class ProductController extends AbstractController
 
         return $this->render('admin/product/new.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/product/{id}/update",
+     *     name="admin_product_update",
+     *     methods={"GET", "PUT"})
+     */
+    public function update(Request $request, Product $product): Response
+    {
+
+        if (!$product) {
+            $this->addFlash('error', 'N\'existe pas');
+
+            return $this->redirectToRoute("admin_product_index");
+        }
+
+        $form = $this->createForm(ProductType::class, $product, [
+            'method' => 'PUT'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                ;
+                $this->em->flush();
+
+                $this->addFlash('success', 'modifié');
+
+                return $this->redirectToRoute("admin_product_index");
+
+            } catch (ORMException $ORMException) {
+                die('erreur');
+            }
+        }
+        return $this->render('admin/product/update.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/{id}/delete",
+     *     name="admin_product_delete",
+     *     methods={"GET", "DELETE"})
+     */
+    public function delete(Request $request, Product $product): Response
+    {
+
+        if (!$product) {
+            $this->addFlash('error', 'N\'existe pas');
+
+            return $this->redirectToRoute("admin_product_index");
+        }
+
+        $defaultData = ['message' => 'Voulez vous effacer ' . $product->getName() . ' ?'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('oui', SubmitType::class)
+            ->add('non', SubmitType::class)
+            ->setMethod('DELETE')
+            ->getForm();
+//dd($form);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('oui')->isClicked()):
+                try {
+                    $this->em->remove($product);
+                    $this->em->flush();
+
+                    $this->addFlash('success', 'supprimé');
+
+                    return $this->redirectToRoute("admin_product_index");
+
+                } catch (ORMException $ORMException) {
+                    die('erreur');
+                }
+            else:
+                $this->addFlash('notice', 'annulé');
+
+                return $this->redirectToRoute("admin_product_index");
+
+            endif;
+
+        }
+        return $this->render('admin/product/delete.html.twig', [
+            'form' => $form->createView(),
+            'default_data' => $defaultData
         ]);
     }
 }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,6 +19,16 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
+
+    public function getAll()
+    {
+        return $this->createQueryBuilder('m')
+            ->orderBy('m.id', 'ASC')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
     // /**
     //  * @return Product[] Returns an array of Product objects
     //  */
