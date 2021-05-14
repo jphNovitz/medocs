55ae538e3cfcec7fb81e99f955644a6d27a62e09
Novitz Jean-Philippe <novitz@gmail.com>
2021-05-14 23:37:12 +0200

    update (DoseType) Correction remove ->getName() on if ->getName() raised an error because if Frequency in null it's impossible

--- src/Form/DoseType.php
+++ src/Form/DoseType.php
@@ -5,14 +5,24 @@ namespace App\Form;
 use App\Entity\Dose;
 use App\Entity\Frequency;
 use App\Entity\Moment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 
 class DoseType extends AbstractType
 {
+
+    protected $em;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->em = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -20,12 +30,48 @@ class DoseType extends AbstractType
                 'class' => Frequency::class,
                 'choice_label' => 'name'
             ])
+            ->add('frequencyNew', FrequencyType::class, array(
+                'required' => FALSE,
+                'mapped' => FALSE,
+                'property_path' => 'item',
+            ))
             ->add('moment', EntityType::class,[
                 'class' => Moment::class,
                 'choice_label' => 'name'
             ])
             ->add('envoi', SubmitType::class)
         ;
+
+       $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
+            $data = $event->getData();
+            $form = $event->getForm();
+//dump($data['frequencyNew']['name']);
+
+//dump($event);
+//dd($data->getFrequency());
+//dd($form);
+//dd($form->get('frequencyNew')->getData()->getName());
+
+            if (!empty($form->get('frequencyNew')->getData())) {
+                $newFrequency = new Frequency();
+                $newFrequency->setName($form->get('frequencyNew')->getData()->getName());
+                $this->em->persist($newFrequency);
+                $this->em->flush();
+              //  $data['frequencyNew']['name'] = '';
+
+                $data->setFrequency($newFrequency);
+                $event->setData($data);
+
+                /*$form->remove('frequency');
+
+                $form->add('frequencyNew', FrequencyType::class, array(
+                    'required' => TRUE,
+                    'mapped' => TRUE,
+                    'property_path' => 'frequency',
+                ));*/
+            }
+        });
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -34,4 +80,6 @@ class DoseType extends AbstractType
             'data_class' => Dose::class,
         ]);
     }
+
+
 }
