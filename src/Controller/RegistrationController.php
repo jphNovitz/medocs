14c3c8d0e0b295b74b7ffcca5c5f73c6577bd96c
Novitz Jean-Philippe <novitz@gmail.com>
2021-08-17 01:41:08 +0200

    fix #23 fix #24

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,6 +30,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_index');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
