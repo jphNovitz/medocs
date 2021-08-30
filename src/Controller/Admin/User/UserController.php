39747978c4f0d3215b51d986042c7768ae9bb332
Novitz Jean-Philippe <novitz@gmail.com>
2021-08-30 04:17:54 +0200

    fix #33

--- src/Controller/Admin/User/UserController.php
+++ src/Controller/Admin/User/UserController.php
@@ -7,6 +7,7 @@ use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +15,11 @@ use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/**
+ * Class UserController
+ * @package App\Controller\Admin\User
+ * @Route("/admin/")
+ */
 class UserController extends AbstractController
 {
     protected $em;
@@ -28,7 +34,7 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/profile/update",
+     * @Route("profile/update",
      *     name="admin_profile_update",
      *     methods={"GET","PUT"})
      */
@@ -42,6 +48,12 @@ class UserController extends AbstractController
         ]);
 
         $form->handleRequest($request);
+
+//        $delete_form = $this->createFormBuilder()
+////            ->add('delete', SubmitType::class)
+//            ->setMethod("DELETE")
+//            ->setAction($this->generateUrl('admin_profile_delete'))
+//            ->getForm();
 
         if ($form->isSubmitted() && $form->isValid()) {
             $email = $form->getData()['email'];
@@ -66,11 +78,50 @@ class UserController extends AbstractController
                 $this->em->flush();
                 $this->addFlash('success', 'modifié');
 
-                return $this->redirectToRoute("admin_index");
+                return $this->redirectToRoute("admin_profile_update");
             } catch (ORMException $ORMException) {
                 die('erreur');
             }
         }
-        return $this->render('admin/profile/update-infos.html.twig', ['form' => $form->createView(),]);
+        return $this->render('admin/profile/update-infos.html.twig', [
+            'form' => $form->createView(),
+//            'form_delete' => $delete_form->createView()
+        ]);
     }
+
+    /**
+     * @Route("profile/delete",
+     *     name="admin_profile_delete",
+     *     methods={"GET", "DELETE"})
+     */
+    public function delete(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createFormBuilder()
+            ->add('yes', SubmitType::class,['translation_domain'=>'messages'])
+            ->add('no', SubmitType::class)
+            ->setMethod("DELETE")
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('yes')->isClicked()) {
+                try {
+                    $this->em->remove($user);
+                    $this->em->flush();
+                    $this->addFlash('success', 'Supprimé');
+
+                    return $this->redirectToRoute("public_index");
+                } catch (ORMException $ORMException) {
+                    die('erreur');
+                }
+            } else return $this->redirectToRoute('admin_profile_update');
+        }
+        return $this->render('admin/profile/delete.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
 }
