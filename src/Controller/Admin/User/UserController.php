3e3059865b4659b7c7397a24530b5912db34afa7
Novitz Jean-Philippe <novitz@gmail.com>
2021-12-06 23:02:44 +0100

    refactor User  controller => remove abstract Controller declaration -- correction

--- src/Controller/Admin/User/UserController.php
+++ src/Controller/Admin/User/UserController.php
@@ -9,31 +9,69 @@ use Doctrine\ORM\ORMException;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Router;
 use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * Class UserController
  * @package App\Controller\Admin\User
  * @Route("/admin/")
  */
-class UserController extends AbstractController
+class UserController
 {
     protected $em;
     protected $encoderFactory;
     protected $passwordEncoder;
+    /**
+     * @var Security
+     */
+    private $security;
+    /**
+     * @var Router
+     */
+    private $router;
+    /**
+     * @var \Twig\Environment
+     */
+    private $twig;
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var FlashBagInterface
+     */
+    private $flashBag;
 
-    public function __construct(EntityManagerInterface $em, EncoderFactoryInterface $encoderFactory, UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(
+        EntityManagerInterface $em,
+        EncoderFactoryInterface $encoderFactory,
+        UserPasswordEncoderInterface $passwordEncoder,
+        Security $security,
+        Router $router,
+        FormFactoryInterface $formFactory,
+        FlashBagInterface $flashBag,
+        \Twig\Environment $twig)
     {
         $this->em = $em;
         $this->encoderFactory = $encoderFactory;
         $this->passwordEncoder = $passwordEncoder;
+        $this->security = $security;
+        $this->router = $router;
+        $this->twig = $twig;
+        $this->formFactory = $formFactory;
+        $this->flashBag = $flashBag;
     }
 
     /**
@@ -43,20 +81,14 @@ class UserController extends AbstractController
      */
     public function updateInfos(Request $request): Response
     {
-        $user = $this->getUser();
+        $user = $this->security->getUser();
 
-        $form = $this->createForm(UserType::class, null, [
-            'action' => $this->generateUrl('admin_profile_update'),
+        $form = $this->formFactory->create(UserType::class, null, [
+            'action' => $this->router->generate('admin_profile_update'),
             'method' => 'PUT'
         ]);
 
         $form->handleRequest($request);
-
-//        $delete_form = $this->createFormBuilder()
-////            ->add('delete', SubmitType::class)
-//            ->setMethod("DELETE")
-//            ->setAction($this->generateUrl('admin_profile_delete'))
-//            ->getForm();
 
         if ($form->isSubmitted() && $form->isValid()) {
             $email = $form->getData()['email'];
@@ -64,7 +96,6 @@ class UserController extends AbstractController
             $repeatPassword = $form->getData()['repeatPassword'];
             if (!empty($plainPassword)) {
                 if ($plainPassword === $repeatPassword) {
-
                     $encoder = $this->encoderFactory->getEncoder(new User());
                     if ($encoder->isPasswordValid($user->getPassword(), $form->getData()['oldPassword'], null)) {
                         $user->setPassword(
@@ -74,22 +105,21 @@ class UserController extends AbstractController
                             )
                         );
                     }
-                } else $this->addFlash('error', 'les deux mots de passes ne sont pas identiques');
+                } else $this->flashBag->add('error', 'les deux mots de passes ne sont pas identiques');
             }
 
             try {
                 $this->em->flush();
-                $this->addFlash('success', 'modifié');
+                $this->flashBag->add('success', 'modifié');
 
-                return $this->redirectToRoute("admin_profile_update");
+                return new RedirectResponse($this->router->generate("admin_profile_update"));
             } catch (ORMException $ORMException) {
                 die('erreur');
             }
         }
-        return $this->render('admin/profile/update-infos.html.twig', [
-            'form' => $form->createView(),
-//            'form_delete' => $delete_form->createView()
-        ]);
+        return new Response($this->twig->render('admin/profile/update-infos.html.twig', [
+            'form' => $form->createView()
+        ]));
     }
 
     /**
@@ -99,14 +129,10 @@ class UserController extends AbstractController
      */
     public function delete(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
-        $user = $this->getUser();
-//        $session = $this->get('session');
-//        $session = new Session();
-//        $session->invalidate();
+        $user = $this->security->getUser();
 
-
-        $form = $this->createFormBuilder()
-            ->add('yes', SubmitType::class,['translation_domain'=>'messages'])
+        $form = $this->formFactory->createBuilder()
+            ->add('yes', SubmitType::class, ['translation_domain' => 'messages'])
             ->add('no', SubmitType::class)
             ->setMethod("DELETE")
             ->getForm();
@@ -116,15 +142,16 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             if ($form->get('yes')->isClicked()) {
                 try {
-//                    dd($this->getUser());
-                    $user_details = $entityManager
-                        ->getRepository(User::class)
-                    ->findUserWithDetails($this->getUser()->getId());
-//                    dd($user_details->getEmail());
-                    $this->get('security.token_storage')->setToken(null);
+                    $user_details = $this->security->getUser();
+//                    $user_details = $entityManager
+//                        ->getRepository(User::class)
+//                        ->findUserWithDetails($this->security->getUser()->getId());
+
+                    $this->security->getToken()->setAuthenticated(false);
+//                    $this->security->get('security.token_storage')->setToken(null);
                     $this->em->remove($user);
                     $this->em->flush();
-                    $this->addFlash('success', 'Supprimé');
+                    $this->flashBag->add('success', 'Supprimé');
 
 
                     // send email
@@ -135,23 +162,20 @@ class UserController extends AbstractController
                         ->htmlTemplate('emails/user/delete-confirmation.html.twig')
 //            ->textTemplate('emails/list/text-list.html.twig')
                         ->context([
-                            'details'=> $user_details
+                            'details' => $user_details
                         ]);
 
-                        $mailer->send($email);
+                    $mailer->send($email);
 
-                    //
-                    return $this->redirectToRoute("public_index");
+                    return new Response($this->router->generate("public_index"));
+
                 } catch (ORMException $ORMException) {
                     dd($ORMException);
-//                    die('erreur');
                 }
-            } else return $this->redirectToRoute('admin_profile_update');
+            } else return new RedirectResponse($this->router->generate('admin_profile_update'));
         }
-        return $this->render('admin/profile/delete.html.twig', [
+        return new Response($this->twig->render('admin/profile/delete.html.twig', [
             'form' => $form->createView()
-        ]);
-
+        ]));
     }
-
 }
