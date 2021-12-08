b70f51ef616e8a81668a0ac356579dc65162c1eb
Novitz Jean-Philippe <novitz@gmail.com>
2021-12-09 00:46:57 +0100

    NEW deleteFormType for generic form type for deletion

---
+++ src/Form/DeleteFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeleteFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('yes', SubmitType::class)
+            ->add('no', SubmitType::class)
+            ->setMethod('DELETE')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
