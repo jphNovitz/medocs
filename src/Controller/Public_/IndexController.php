c883fa81a06d3e85e2945db69773ae717bea0b86
Novitz Jean-Philippe <novitz@gmail.com>
2024-07-14 23:52:35 +0200

    Refactor public index controller
    - Replace annotations by attributes
    - controller extends abstract controller

--- src/Controller/Public_/IndexController.php
+++ src/Controller/Public_/IndexController.php
@@ -13,66 +13,40 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Security;
 
-class IndexController
+class IndexController extends AbstractController
 {
-    /**
-     * @var Security
-     */
-    private $security;
-    /**
-     * @var \Twig\Environment
-     */
-    private $twig;
-    /**
-     * @var Router
-     */
-    private $router;
 
-    public function __construct(Security $security,
-                                \Twig\Environment $twig,
-                                RouterInterface $router)
-    {
-        $this->security = $security;
-        $this->twig = $twig;
-        $this->router = $router;
-    }
-
-    /**
-     * @Route("/", name="public_index")
-     */
+    #[Route('/', name: 'public_index')]
     public function index(): Response
     {
-        if ($this->security->getUser()) {
-            return new RedirectResponse($this->router->generate('admin_index'));
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_index');
         }
-        return new Response($this->twig->render('public/index/index.html.twig'));
+        return $this->render('public/index/index.html.twig', []);
     }
 
-    /**
-     * @Route("/g/{slug}", name="public_page")
-     */
+    #[Route('/g/{slug}', name: 'public_page')]
     public function publicUrl(User $user): Response
     {
-        return new Response($this->twig->render('public/index/list.html.twig', [
+        return $this->render('public/index/list.html.twig', [
             'list' => $user->getLines(),
-        ]));
+        ]);
     }
 
-
-    /**
-     * @Route("/condition-utilisation", name="public_condition_use_fr")
-     */
+    #[Route('/condition-utilisation', name: 'public_condition_use_fr')]
     public function condition_use(): Response
     {
-        return new Response($this->twig->render('public/index/conditions-use_fr.html.twig'));
+        return $this->render('public/index/conditions-use_fr.html.twig');
     }
 
-    /**
-     * @Route("/foire-aux-questions", name="public_faq_fr")
-     */
-    public function faq(): Response
-    {
-        return new Response($this->twig->render('public/index/faq_fr.html.twig'));
-    }
+//    /**
+//     * @Route("/foire-aux-questions", name="public_faq_fr")
+//     */
+//    #[Route('/foire-aux-questions', name: 'public_faq_fr')]
+//
+//    public function faq(): Response
+//    {
+//        return $this->render('public/index/faq_fr.html.twig');
+//    }
 
 }
