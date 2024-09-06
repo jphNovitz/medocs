1020200e3db4701c5c96c06495b2d79cef481e5e
Novitz Jean-Philippe <novitz@gmail.com>
2024-09-06 11:27:30 +0200

    Refactor product Repository
    complete  field details

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,21 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('comment')
-            //->add('envoi', SubmitType::class)
+            ->add('name', TextType::class, [
+                'translation_domain' => 'messages',
+                'label' => 'form.product.name',
+                'attr' => [
+                    'placeholder' => 'form.name.placeholder',
+                ]
+            ])
+            ->add('comment', TextType::class, [
+                'translation_domain' => 'messages',
+                'label' => 'form.product.comment',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'form.name.comment',
+                ]
+            ])//->add('envoi', SubmitType::class)
         ;
     }
 
