bf379cebe4af02a5e7c82a5701c8e20d9221f0bc
Novitz Jean-Philippe <novitz@gmail.com>
2025-04-06 01:02:11 +0200

    Refactor Dose and Product controllers to simplify constructors and remove unused methods

--- src/Controller/Member/Dose/DoseController.php
+++ src/Controller/Member/Dose/DoseController.php
@@ -18,44 +18,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/dose')]
+#[Route('/member/dose')]
 class DoseController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $em,
-                                private DoseRepository         $doseRepository)
+    public function __construct(private EntityManagerInterface $em)
     {}
-
-    #[Route('', name: 'member_dose_index', methods: ['GET'])]
-    public function index(): Response
-    {
-        return $this->render('admin/dose/dose/index.html.twig', [
-            'list' => $this->doseRepository->getAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'member_dose_new', methods: ['GET', 'POST'])]
-    public function create(Request $request): Response
-    {
-
-        $dose = new Dose();
-        $form = $this->createForm(DoseType::class, $dose);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-          $this->em->persist($dose);
-                $this->em->flush();
-
-                $this->addFlash('success', 'ajouté');
-
-                $previousPage = $request->getSession()->get('previous_page', $this->generateUrl('member_dose_new'));
-                $request->getSession()->remove('previous_page');
-
-        }
-        return $this->render('admin/dose/dose/new.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
 
     #[Route('/api/new', name: 'member_dose_api_new', methods: ['POST'])]
     public function new(Request $request): Response
@@ -95,65 +62,7 @@ class DoseController extends AbstractController
         ], 200);
 
     }
-    
-    #[Route('/{id}/update', name: 'member_dose_update', methods: ['GET', 'PUT'])]
-    public function update(Request $request, Dose $dose = null): Response
-    {
-        if (!$dose) {
-            $this->addFlash('error', 'N\'existe pas');
 
-            return $this->redirectToRoute("member_dose_index");
-        }
-
-        $form = $this->createForm(DoseType::class, $dose, [
-            'method' => 'PUT'
-        ]);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-                $this->em->flush();
-
-                $this->addFlash('success', 'modifié');
-
-                return $this->redirectToRoute("member_dose_index");
-        }
-        return $this->render('admin/dose/dose/update.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/{id}/delete', name: 'member_dose_delete', methods: ['GET', 'DELETE'])]
-    public function delete(Request $request, Dose $dose= null): Response
-    {
-        if (!$dose) {
-            $this->addFlash('error', 'N\'existe pas');
-
-            return $this->redirectToRoute("member_dose_index");
-        }
-
-        $defaultData = ['message' => 'Voulez vous effacer ' . $dose . ' ?'];
-
-        $form = $this->createForm(DeleteFormType::class, $defaultData);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($form->get('yes')->isClicked()):
-                    $this->em->remove($dose);
-                    $this->em->flush();
-
-                    $this->addFlash('success', 'supprimé');
-                    return $this->redirectToRoute("member_dose_index");
-            else:
-                $this->addFlash('notice', 'annulé');
-                return $this->redirectToRoute("member_dose_index");
-            endif;
-        }
-        return $this->render('member/dose/dose/delete.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
 
 
 }
--- src/Controller/Member/Product/ProductController.php
+++ src/Controller/Member/Product/ProductController.php
@@ -24,59 +24,8 @@ use Symfony\Component\Security\Core\Security;
 #[Route('/member/product')]
 class ProductController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $entityManager,
-                                private ProductRepository      $productRepository,
-                                UserRepository                 $userRepository)
-    {
-    }
-
-    #[Route('/', name: 'member_product_index')]
-    public function index(): Response
-    {
-        if (!$list = $this->productRepository->getAll())
-            return $this->redirectToRoute('member_product_new');
-
-        return $this->render('admin/product/list/index.html.twig', [
-            'list' => $list
-        ]);
-    }
-
-    #[Route('/new', name: 'member_product_new')]
-    public function new(Request $request): Response
-    {
-        dd($this->entityManager->getRepository(Product::class)->findAll());
-        $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
-
-//        if (!$request->isMethod('POST')) {
-//            $request->getSession()->set('previous_page', $request->headers->get('referer'));
-//        }
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($product);
-            $this->entityManager->flush();
-            $this->addFlash('success', 'Produit ajouté');
-
-            if ($referer = $request->getSession()->get('referer')) {
-                $request->getSession()->remove('referer');
-                return $this->redirectToRoute($referer);
-            } else
-                $referer = $request->headers->get('referer');
-
-            // Vérifier si le referer existe et rediriger
-            $previousPage = $request->getSession()->get('previous_page', $this->generateUrl('member_product_new'));
-
-            // Supprimer l'URL de la session
-            $request->getSession()->remove('previous_page');
-
-            return $this->redirect($previousPage);
-        }
-
-        return new Response($this->render('member/product/new.html.twig', [
-            'form' => $form->createView()
-        ]));
-    }
+    public function __construct(private EntityManagerInterface $entityManager)
+    {}
 
     #[Route('/api/new', name: 'member_product_api_new', methods: ["POST"])]
     public function newApi(Request $request): Response
@@ -105,76 +54,5 @@ class ProductController extends AbstractController
                 'message' => 'Données non enregistrées.',
             ], 400);
         }
-    }
-
-    #[Route('/{id}/update', name: 'member_product_update', methods: ["GET", "PUT"])]
-    public function update(Request $request, Product $product = null): Response
-    {
-
-        if (!$product) {
-            $this->addFlash('error', 'N\'existe pas');
-            return $this->redirectToRoute("member_product_index");
-        }
-
-        $form = $this->createForm(ProductType::class, $product, [
-            'method' => 'PUT'
-        ]);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            try {
-                $this->entityManager->flush();
-                $this->addFlash('success', 'modifié');
-                return $this->redirectToRoute("member_product_index");
-
-            } catch (DriverException $exception) {
-                die('erreur');
-            }
-        }
-        return $this->render('admin/product/update.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/{id}/delete', name: 'member_product_delete', methods: ["GET", "DELETE"])]
-    public function delete(Request $request, Product $product = null): Response
-    {
-
-        if (!$product) {
-            $this->addFlash('error', 'N\'existe pas');
-
-            return $this->redirectToRoute("member_product_index");
-        }
-
-        $defaultData = ['message' => 'Voulez vous effacer ' . $product->getName() . ' ?'];
-        $form = $this->createFormBuilder(null, $defaultData)
-            ->add('yes', SubmitType::class, ['label' => 'Oui Supprimer'])
-            ->add('no', SubmitType::class, ['label' => 'Non Annuler'])
-            ->setMethod('DELETE')
-            ->getForm();
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($form->get('yes')->isClicked()):
-                try {
-                    $this->entityManager->remove($product);
-                    $this->entityManager->flush();
-                    $this->addFlash('success', 'supprimé');
-
-                    return $this->redirectToRoute("member_product_index");
-
-                } catch (DriverException $exception) {
-                    die('erreur');
-                }
-            else:
-                $this->addFlash('notice', 'annulé');
-                return $this->redirectToRoute("member_product_index");
-            endif;
-        }
-        return $this->render('admin/product/delete.html.twig', [
-            'form' => $form->createView()
-        ]);
     }
 }
